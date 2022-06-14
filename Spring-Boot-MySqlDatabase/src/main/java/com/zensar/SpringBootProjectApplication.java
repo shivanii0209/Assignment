a3293f4cee68a5013b23ac74b95cba0021751a93
Shivani Mane <68547323+shivanii0209@users.noreply.github.com>
2022-06-14 17:38:46 +0530

    Add files via upload

---
+++ Spring-Boot-MySqlDatabase/src/main/java/com/zensar/SpringBootProjectApplication.java
@@ -0,0 +1,20 @@
+package com.zensar;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"test","com.zensar"})
+public class SpringBootProjectApplication {
+
+	public static void main(String...args ) {
+	System.out.println("Ram");
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+		System.out.println("Laxman");
+	}
+
+
+
+}
